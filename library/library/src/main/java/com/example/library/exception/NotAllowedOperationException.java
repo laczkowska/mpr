97f9a693548fd98752653daa845d1afd686e8d4f
Natalia Łączkowska <91725304+laczkowska@users.noreply.github.com>
2024-01-19 11:23:11 +0100

    Add files via upload

---
+++ library/library/src/main/java/com/example/library/exception/NotAllowedOperationException.java
@@ -0,0 +1,8 @@
+package com.example.library.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class NotAllowedOperationException extends RuntimeException{
+}
