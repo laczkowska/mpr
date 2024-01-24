b96a2e5c97a31dc2fcc750c16b19f81e06060c36
Natalia <s24545@pjwstk.edu.pl>
2024-01-24 19:48:59 +0100

    git

--- projekt/src/main/java/org/example/dto/BookMapper.java
+++ projekt/src/main/java/org/example/dto/BookMapper.java
@@ -2,9 +2,13 @@ package org.example.dto;
 
 import org.example.data.Book;
 import org.example.dto.BookDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface BookMapper {
 
     BookDTO bookToBookDTO(Book book);
+
+
 }
